2a01c5512a883b30b9f45ed9e623e0daccee86c4
AhmedMeshaal <ahmedmeshaal.66@gmail.com>
2024-10-16 13:49:50 +0300

    Enable Create User

--- actions/register.ts
+++ actions/register.ts
@@ -1,7 +1,12 @@
 "use server"
 
 import * as z from "zod";
+import bcrypt from "bcrypt";
+
 import { RegisterSchema } from "@/schemas";
+import { db } from "@/lib/db";
+import {bgCyan} from "next/dist/lib/picocolors";
+import {getUserByEmail} from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validateFields = RegisterSchema.safeParse(values);
@@ -9,5 +14,23 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
      if (!validateFields.success) {
          return { error: "Invalid fields!" };
      }
-     return { success: "Email sent!" };
+
+     const { email, password, name } = validateFields.data;
+     const hashedPassword = await bcrypt.hash(password, 10);
+
+     const existingUser = await getUserByEmail(email);
+
+      if (existingUser) {
+          return { error: "Email Already in use!" };
+      }
+
+      await db.user.create({
+          data: {
+              name,
+              email,
+              password: hashedPassword
+          },
+      });
+
+     return { success: "User has been created!" };
 };
