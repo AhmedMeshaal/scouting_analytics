cccfa621eb679fc58946e7a49b1c2541de76b5cc
AhmedMeshaal <ahmedmeshaal.66@gmail.com>
2024-10-31 12:20:08 +0300

    Difference between Client - Server And Create Access Rights for Admin

---
+++ app/(protected)/server/page.tsx
@@ -0,0 +1,15 @@
+import {auth} from "@/auth";
+import {currentUser} from "@/lib/auth";
+import {UserInfo} from "@/components/user-info";
+
+const ServerPage = async () => {
+    const user = await currentUser();
+    return (
+        <UserInfo
+            label="server component"
+            user={user}
+        />
+    )
+}
+
+export default ServerPage;
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import { SessionProvider } from "next-auth/react";
 import { auth } from "@/auth";
+import { Toaster } from "@/components/ui/sonner"
+
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -23,6 +25,7 @@ export default async function RootLayout({
       <SessionProvider session={session}>
         <html lang="en">
           <body className={inter.className}>
+          <Toaster />
             {children}
           </body>
         </html>
